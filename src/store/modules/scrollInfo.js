be7f5cc6b6c298f1577ef80229b6c066418cb414
chenxuewei <chen.xuewei@yilutong.com>
2019-10-04 13:26:57 +0800

    滚动的问题 暂时未解决

--- src/store/modules/scrollInfo.js
+++ src/store/modules/scrollInfo.js
@@ -1,17 +1,36 @@
 
+
+// 给mutations的方法定义别名
+const mutationTypes = {
+  // 设置headerOpacity的值 setHeaderOpacity
+  SETHEADEROPACITY: 'SETHEADEROPACITY',
+};
+
 function getInitState() {
   return {
     isScrolling: false,
     timeOut: null,
+    headerOpacity: 0,
   }
 }
 let actions = {
   onScroll({commit, state}) {
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    console.log('监听滚动事件', scrollTop);
+    if (scrollTop > 100) {
+      let value =  (scrollTop-100)/10;
+      commit(mutationTypes.SETHEADEROPACITY, value);
+    } else if (scrollTop < 100 && scrollTop > 90) {
+      let value = (10-(100 - scrollTop))/10;
+      commit(mutationTypes.SETHEADEROPACITY, value);
+    }
   }
 }
-let mutations = {}
+let mutations = {
+  [mutationTypes.SETHEADEROPACITY](state, value) {
+    console.log('value',value);
+    state.headerOpacity = value;
+  }
+}
 let getters = {}
 let module = {
   namespaced: true,
